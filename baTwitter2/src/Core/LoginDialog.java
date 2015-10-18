7893763c24a7ebbeb6cafb9dbb09ad2cdc54efd8
Ray <orturugbylari0840@gmail.com>
2015-10-18 14:45:56 +0200

    Lehioa Aldaketa, 1.Lehioa remodelatu

--- baTwitter2/src/Core/LoginDialog.java
+++ baTwitter2/src/Core/LoginDialog.java
@@ -19,30 +19,19 @@ class LoginDialog extends JDialog implements ActionListener {
     JDialog dialog;
 
     public LoginDialog() {
-        JPanel panelOne = new JPanel();
-        labelName = new JLabel("Name");
-        textName = new JTextField(15);
-        panelOne.add(labelName);
-        panelOne.add(textName);
+       
 
-        JPanel panelTwo = new JPanel();
-        labelPass = new JLabel("Password");
-        passField = new JPasswordField(15);
-        panelTwo.add(labelPass);
-        panelTwo.add(passField);
+        
 
         JPanel panelThree = new JPanel();
         okButton = new JButton("OK");
-        cancelButton = new JButton("Cancel");
         okButton.addActionListener(this);
-        cancelButton.addActionListener(this);
+        //okButton.
         panelThree.add(okButton);
         panelThree.add(cancelButton);
 
         dialog = new JDialog();
         dialog.setResizable(false);
-        dialog.getContentPane().add(panelOne);
-        dialog.getContentPane().add(panelTwo);
         dialog.getContentPane().add(panelThree);
         dialog.setTitle("Login in to Twitter");
         dialog.getContentPane().setLayout(new FlowLayout());
@@ -72,10 +61,7 @@ class LoginDialog extends JDialog implements ActionListener {
     
     public static void main(String[] args) {
         LoginDialog login = new LoginDialog();
-        String userName = login.getUserName();
-        String password = login.getPassword();
-
-        
+           
            // Twitter twitter = new Twitter(userName, password); b
            // twitter.verifyCredentials();
             JOptionPane.showMessageDialog(login, "Login successful!");
