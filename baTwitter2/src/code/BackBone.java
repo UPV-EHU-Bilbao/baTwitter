2901220a3b03f79a23d597ef51309771a16caaf0
Ray <orturugbylari0840@gmail.com>
2015-11-04 11:13:35 +0100

    getTimeline, done

--- baTwitter2/src/code/BackBone.java
+++ baTwitter2/src/code/BackBone.java
@@ -9,7 +9,6 @@ import java.util.*;
 import twitter4j.Status;
 
 public class BackBone {
-	Twitter twitter;
 	ArrayList<Status> Timeline;
 	
 	
@@ -17,12 +16,34 @@ public class BackBone {
 	}
 	
 	public void getTimeline() throws TwitterException{
-		//First param of Paging() is the page number, second is the number per page (this is capped around 200 I think.
+		
+		int pageno=1;
+		List statuses = new ArrayList();
+		
+		while (true){
+			try{
+				int size = statuses.size();
+				Paging page = new Paging(pageno++,100);
+				statuses.addAll(LoginCode.nireLoginCode.twitter.getHomeTimeline(page));
+				if (statuses.size()==size){
+					break;
+				}
+			}catch (TwitterException e){
+				e.printStackTrace();
+			}
+		}
+		Iterator<Status> itr = statuses.iterator();
+		while (itr.hasNext()){
+			System.out.println(itr.next().getText());
+		}
+		System.out.println(statuses.size());
+		
+		/*First param of Paging() is the page number, second is the number per page (this is capped around 200 I think.
 		Paging paging = new Paging(1, 200);
 		ResponseList<Status> statuses = LoginCode.nireLoginCode.twitter.getHomeTimeline(paging);
 		for(int i=0;i<statuses.size();i++){
 			System.out.println(statuses.get(i).getText());
-		}
+		}*/
 	}
 	
 	
