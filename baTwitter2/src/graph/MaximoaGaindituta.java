d52c9db6e276ecd303271bf51278f642ef10f6d3
bingen <zurrrakapote@gmail.com>
2015-11-10 20:54:40 +0100

    getTweet

---
+++ baTwitter2/src/graph/MaximoaGaindituta.java
@@ -0,0 +1,54 @@
+package graph;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextPane;
+import javax.swing.JTextField;
+
+public class MaximoaGaindituta extends JFrame {
+
+	private JPanel contentPane;
+	/**
+	 * @wbp.nonvisual location=49,47
+	 */
+	private final JTextField textField = new JTextField();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MaximoaGaindituta frame = new MaximoaGaindituta();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MaximoaGaindituta() {
+		textField.setColumns(10);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 250, 140);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(191, 43, -95, 60);
+		//textPane.setText(Integer.toString(i));
+		
+		
+	}
+}
