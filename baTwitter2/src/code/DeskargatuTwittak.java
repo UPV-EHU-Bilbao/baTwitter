ba3fdc1bc089f7954da772ebcada07b400b48fda
bingen <zurrrakapote@gmail.com>
2015-11-22 13:27:30 +0100

    Printzipioz ondo funztionatzen du, jarraitzaileak deskargatzearena.

--- baTwitter2/src/code/DeskargatuTwittak.java
+++ baTwitter2/src/code/DeskargatuTwittak.java
@@ -9,13 +9,17 @@ public class DeskargatuTwittak {
 	
 	
 	
-	public static void  main(String[] args) {
+	public static void  main(String[] args) throws TwitterException {
 		
 		ConfigurationBuilder cb = new ConfigurationBuilder();
 		cb.setOAuthAccessToken("368286014-qLSe2tzmXV934K3FBcXuhILwoYeOQd3C9EOQMrAQ");
 		cb.setOAuthAccessTokenSecret("TLrGCtG976ZNDSQyDmtwKZ17SyXyrjBlBvfZBP9Qff2es");
 		cb.setOAuthConsumerKey("VZui0P0P00DX1q9SeCxLlSDYv");
 	    cb.setOAuthConsumerSecret("ZvlLujot49kqG6qd0SJp3PLFRyHUIp9XtmEw6bYOlOmqpFC1F1");
+	    FollowerANDFollowed f= new FollowerANDFollowed(cb);
+	    
+	   
+	    /*
 		Twitter twitter = new TwitterFactory(cb.build()).getInstance();
 		int pagenum= 1;
 		String usr= "bingenzio";
@@ -57,7 +61,8 @@ public class DeskargatuTwittak {
 		}
 	
 		// TODO Auto-generated method stub
-
+*/
+		
 	}
 
 }
--- baTwitter2/src/code/FollowerANDFollowed.java
+++ baTwitter2/src/code/FollowerANDFollowed.java
@@ -1,55 +1,59 @@
 package code;
 
-import java.util.ArrayList;
+
 
 import twitter4j.IDs;
+
+import twitter4j.ResponseList;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.User;
 import twitter4j.conf.ConfigurationBuilder;
+
 
 public class FollowerANDFollowed {
 
 	//private ArrayList<String> followed= new ArrayList<String>();
 	//private ArrayList<String> following= new ArrayList<String>();
-	//private static Twitter t;
+	private static Twitter t;
+	
 	public FollowerANDFollowed(ConfigurationBuilder cb){
-		// t = new TwitterFactory(cb.build()).getInstance();
+		 t = new TwitterFactory(cb.build()).getInstance();
 	}
 
 	
-
-	 public static void main(String[] args) {
-			ConfigurationBuilder cb = new ConfigurationBuilder();
+	  
+	  
+	   public static void main(String[] args) {
+		   ConfigurationBuilder cb = new ConfigurationBuilder();
 			cb.setOAuthAccessToken("368286014-qLSe2tzmXV934K3FBcXuhILwoYeOQd3C9EOQMrAQ");
 			cb.setOAuthAccessTokenSecret("TLrGCtG976ZNDSQyDmtwKZ17SyXyrjBlBvfZBP9Qff2es");
 			cb.setOAuthConsumerKey("VZui0P0P00DX1q9SeCxLlSDYv");
 		    cb.setOAuthConsumerSecret("ZvlLujot49kqG6qd0SJp3PLFRyHUIp9XtmEw6bYOlOmqpFC1F1");
-			Twitter t = new TwitterFactory(cb.build()).getInstance();
 	        try {
-	          //  Twitter twitter = new TwitterFactory().getInstance();
-	        	
-	            long cursor = 1;
+	    		Twitter twitter = new TwitterFactory(cb.build()).getInstance();
+	            long cursor = -1;
 	            IDs ids;
-	            System.out.println("Listing followers's ids.");
+	            System.out.println("Listing following ids.");
 	            do {
 	                if (0 < args.length) {
-	                    ids = t.getFollowersIDs(args[0], cursor);
-	                    
-	                    System.out.println(ids);
-	                    //t.getFollowersList(args[0],cursor);
+	                    ids = twitter.getFriendsIDs(args[0], cursor);
 	                } else {
-	                    ids = t.getFollowersIDs(cursor);
+	                    ids = twitter.getFriendsIDs(cursor);
+	                    
 	                }
-	                for (long id : ids.getIDs()) {
-	                    System.out.println(id);
-	                }
+	                final ResponseList<User> users = t.lookupUsers(ids.getIDs());
+	                //for (User u : users) {
+	              System.out.println(users.get(0).getName());
+	                   // System.out.println(u.getScreenName());
+	               // }
 	            } while ((cursor = ids.getNextCursor()) != 0);
 	            System.exit(0);
 	        } catch (TwitterException te) {
 	            te.printStackTrace();
-	            System.out.println("Failed to get followers' ids: " + te.getMessage());
+	           System.out.println(te.getRateLimitStatus());
 	            System.exit(-1);
 	        }
-	 }
+	    }
 	 }
